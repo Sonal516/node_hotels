a31abd9e1a389fb23b238a95bdc0dd2bb00c64cc
Sonal <sonal7122024@gmail.com>
2026-05-19 14:59:06 +0530

    Initial clean setup

--- db.js
+++ db.js
@@ -1,9 +1,13 @@
 const mongoose = require('mongoose');
+require('dotenv').config();
 
 //define the mongodb connection url
-const mongoURL = 'mongodb://localhost:27017/hotels';// replace mydatabase with your database name
+//const mongoURL = 'mongodb://localhost:27017/hotels';// replace mydatabase with your database name
 
+//const mongoURL = 'mongodb://userid:<password>@ac-u24qxc8-shard-00-00.jxlozh4.mongodb.net:27017,ac-u24qxc8-shard-00-01.jxlozh4.mongodb.net:27017,ac-u24qxc8-shard-00-02.jxlozh4.mongodb.net:27017/?ssl=true&replicaSet=atlas-p3jekw-shard-0&authSource=admin&appName=Cluster0';
 //setup mongoDB connection
+//const mongoURL = process.env.MONGODB_URL_LOCAL;
+const mongoURL = process.env.MONGODB_URL;
 mongoose.connect(mongoURL);
 
 //get the default connection
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const db = require('./db');
+require('dotenv').config();
 
 const bodyParser = require('body-parser');
 app.use(bodyParser.json());
@@ -19,6 +20,8 @@ app.use('/',personRoutes);
 
 const menuRoutes = require('./routes/menuRoutes');
 app.use('/',menuRoutes);
+
+const PORT = process.env.PORT || 3000;
 
 app.listen(3000, () => {
   console.log('Server is running on http://localhost:3000')
